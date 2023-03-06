afa41ee6a48439d9def6fd11f91a96a343a7696c
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-03-06 20:32:24 +0100

    ccc

--- test1.py
+++ test1.py
@@ -2,6 +2,7 @@ import math
 from punto import Punto
 
 def test_crear_punto():
+    print("Comprobando la creación de un punto...")
     punto = Punto()
     assert punto.x == 0
     assert punto.y == 0
@@ -11,10 +12,12 @@ def test_crear_punto():
     assert punto.y == 4
     
 def test_str():
+    print("Comprobando la representación de un punto...")
     punto = Punto(3, 4)
     assert str(punto) == "(3,4)"
     
 def test_cuadrante():
+    print("Comprobando el cuadrante de un punto...")
     punto = Punto(3, 4)
     assert punto.cuadrante() == "El punto (3,4) pertenece al primer cuadrante"
     
@@ -37,11 +40,13 @@ def test_cuadrante():
     assert punto.cuadrante() == "El punto (0,0) está sobre el origen"
     
 def test_vector():
+    print("Comprobando el vector entre dos puntos...")
     punto1 = Punto(3, 4)
     punto2 = Punto(-1, 2)
     assert punto1.vector(punto2) == "El vector resultante entre (3,4) y (-1,2) es (-4,-2)"
     
 def test_distancia():
+    print("Comprobando la distancia entre dos puntos...")
     punto1 = Punto(3, 4)
     punto2 = Punto(-1, 2)
     assert math.isclose(punto1.distancia(punto2), 5.0, rel_tol=1e-9)
